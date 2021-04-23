b441c68d442f532fd6328b34ab89c2a45d75455e
Dharmang92 <dharmang.gajjar92@gmail.com>
2021-04-23 20:24:44 +0530

    added new components and other changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterOutlet} from "@angular/router";
+import {animate, group, query, style, transition, trigger} from "@angular/animations";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  animations: [
+    trigger("routeAnimation", [
+      transition("* => *", [
+        style({
+          position: "relative"
+        }),
+        query(":enter, :leave", [
+          style({
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            display: "block"
+          })
+        ], {optional: true}),
+        query(":enter", [
+          style({
+            opacity: 0,
+            height: "100%"
+          })
+        ], {optional: true}),
+        group([
+          query(":leave", [
+            animate(200, style({
+              opacity: 0
+            }))
+          ], {optional: true}),
+          query(":enter", [
+            animate(200, style({
+              opacity: 1
+            }))
+          ], {optional: true})
+        ])
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'angular-dashboard';
+  prepareRouter(outlet: RouterOutlet): any {
+    if (outlet.isActivated) {
+      return outlet.activatedRoute.snapshot.url;
+    }
+  }
 }
---
+++ src/app/tabs/tabs.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-tabs",
+  templateUrl: "./tabs.component.html",
+  styleUrls: ["./tabs.component.scss"]
+})
+export class TabsComponent implements OnInit {
+  tabList: string[] = ["bookmarks", "todos", "notes"];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
